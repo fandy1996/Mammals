e9f76464e703a4a91f31beb43e3d6c6a83284ba4
Fandi Shen <fandy@umich.edu>
2017-10-17 11:52:31 -0400

    Added echo filename

--- do-uniq-species.sh
+++ do-uniq-species.sh
@@ -1,6 +1,8 @@
 # Purpose: find the unique species in a dataset
 # Date: Tuesday. Oct 17, 2017
 # Author: Fandy
-
+# Wow it's so cool to use git
 # Loop over all files 
 for data_file in $@
+	do 
+	echo "Unique species in $data_file"
